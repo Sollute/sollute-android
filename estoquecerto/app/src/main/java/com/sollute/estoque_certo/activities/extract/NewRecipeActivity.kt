85198e5af2313e637b511d9bd4d7f689e7a3acb2
GuilhermeAlmeida0602 <xleunamx11097@gmail.com>
2022-09-25 18:22:26 -0300

    feature/create-product-screen/add-create-extract-recipe-and-expenses-screens

---
+++ estoquecerto/app/src/main/java/com/sollute/estoque_certo/activities/extract/NewRecipeActivity.kt
@@ -0,0 +1,17 @@
+package com.sollute.estoque_certo.activities.extract
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sollute.estoque_certo.databinding.ActivityNewRecipeBinding
+
+class NewRecipeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityNewRecipeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNewRecipeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
