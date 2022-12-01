2412180cfd0b9a6e54c95ae28e4eef5a2562c40c
GuilhermeAlmeida0602 <xleunamx11097@gmail.com>
2022-12-01 15:30:35 -0300

    feature/create-endpoint-to-list-employees

---
+++ estoquecerto/app/src/main/java/com/sollute/estoque_certo/models/employee/ListEmployee.kt
@@ -0,0 +1,9 @@
+package com.sollute.estoque_certo.models.employee
+
+data class ListEmployee(
+    val idFuncionario: Int,
+    val nomeFuncionario: String,
+    val cpfFuncionario: String,
+    val telefoneFuncionario: String,
+    val salarioFuncionario : Double
+)
